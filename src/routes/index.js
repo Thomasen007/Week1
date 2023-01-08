472a565ab44810a5014ddf865a15084c2470ddae
Thomasen007 <Thomas.elliott.nielson@gmail.com>
2023-01-07 21:16:29 -0700

    More pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
-
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
   res.send('Thomas Nielson');
 });
 routes.get('/test', (req, res) => {
-  res.send('This is a second page');
+  res.send('Briana Nielson');
+});
+routes.get('/test2', (req, res) => {
+  res.send('Tobias Nielson');
 });
 
 module.exports = routes;
