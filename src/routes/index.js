085f646da5b84fe0fc49056b783589f9fb81ef2d
Thomasen007 <Thomas.elliott.nielson@gmail.com>
2023-01-07 21:08:53 -0700

    Second test page

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,8 @@ const routes = require('express').Router();
 routes.get('/', (req, res) => {
   res.send('Thomas Nielson');
 });
+routes.get('/test', (req, res) => {
+  res.send('This is a second page');
+});
 
 module.exports = routes;
